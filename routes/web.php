a97a230029a738e8de934daadf492c3a63bc3fb4
John Cape <139086976+Donnerpartyplatter@users.noreply.github.com>
2024-12-03 11:38:39 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,59 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Models\Task;
-use Illuminate\Http\Request;
+/**
+ * Show Registration Form
+ */
+Route::get('/register', [StudentController::class, 'create']);
 
 /**
-    * Show Task Dashboard
-    */
-Route::get('/', function () {
-    Log::info("Get /");
-    $startTime = microtime(true);
-    // Simple cache-aside logic
-    if (Cache::has('tasks')) {
-        $data = Cache::get('tasks');
-        return view('tasks', ['tasks' => $data, 'elapsed' => microtime(true) - $startTime]);
-    } else {
-        $data = Task::orderBy('created_at', 'asc')->get();
-        Cache::add('tasks', $data);
-        return view('tasks', ['tasks' => $data, 'elapsed' => microtime(true) - $startTime]);
-    }
-});
-
-/**
-    * Add New Task
-    */
-Route::post('/task', function (Request $request) {
-    Log::info("Post /task");
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        Log::error("Add task failed.");
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-    // Clear the cache
-    Cache::flush();
-
-    return redirect('/');
-});
-
-/**
-    * Delete Task
-    */
-Route::delete('/task/{id}', function ($id) {
-    Log::info('Delete /task/'.$id);
-    Task::findOrFail($id)->delete();
-    // Clear the cache
-    Cache::flush();
-
-    return redirect('/');
-});
+ * Handle Registration Form Submission
+ */
+Route::post('/register', [StudentController::class, 'store']);
